c53694555f44631790db7ec1cb97267921307552
Jeferson-home <phayne693@gmail.com>
2023-07-01 11:27:09 -0300

    test

--- api/index.js
+++ api/index.js
@@ -2,9 +2,16 @@ import express from 'express';
 import supertest from 'supertest';
 import dotenv from 'dotenv';
 import { runTest } from '../login/tk_project.js'
+import supertest from "supertest";
+import dotenv from 'dotenv'
+import { describe, it } from 'mocha'; // Importe a biblioteca Mocha
+dotenv.config()
+
+const request = supertest('https://api.norwaydigital.com.br/prod/v1');
+
 
 const app = express();
-const request = supertest(app);
+
 
 dotenv.config();
 
@@ -22,6 +29,27 @@ app.get('/', async (req, res) => {
       success:true,
       message:"Sucesso."
     })
+});
+
+app.get('/test', async (req, res) => {
+  describe('Users', () => {
+
+    it('POST /TK-PROJECT', (done) => {
+      let data = {
+        "secretKey": process.env.SECRET_KEY,
+        "boundId": process.env.BOUND_ID
+      }
+
+      request.post(`/oauth/projects`).send(data).end((err, response) => {
+        if (err) {
+          done(err);
+        } else {
+          const token = response.body.data.token;
+          done();
+        }
+      });
+    });
+  });
 });
 
 app.listen(3000, () => {
